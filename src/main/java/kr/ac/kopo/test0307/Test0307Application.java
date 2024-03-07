7e607ba42ac8783db7dfd57bec227c6677e78e3b
SeokHoL <sukho8407@gmail.com>
2024-03-07 11:29:56 +0900

    @RestController

--- src/main/java/kr/ac/kopo/test0307/Test0307Application.java
+++ src/main/java/kr/ac/kopo/test0307/Test0307Application.java
@@ -2,9 +2,13 @@ package kr.ac.kopo.test0307;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @SpringBootApplication
 public class Test0307Application {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Test0307Application.class, args);
